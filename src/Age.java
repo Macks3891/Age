fdad2e5d8d4f040e2fa5828aba60803c7f94a6e5
Максим <166473866+Macks3891@users.noreply.github.com>
2024-09-11 23:15:39 +0500

    Age new1 Commit

--- src/Age.java
+++ src/Age.java
@@ -1,15 +1,27 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Age {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner agePerson = new Scanner(System.in);
+        String message = "Введите возраст человека.";
+        System.out.println(message);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        int age = agePerson.nextInt();
+
+        if (age >= 2 && age <= 6) {
+            message = "Иди в детский сад.";
+        } else if (age >= 7 && age <= 18) {
+            message = "Иди в школу.";
+        } else if (age > 18 && age < 24) {
+            message = "Уже следует учиться в универе.";
+        } else if (age >= 24) {
+            message = "Пора на работу.";
+        } else {
+            message = "Некорректный возраст.";
         }
+
+        System.out.println(message);
+
+        agePerson.close();
     }
 }
