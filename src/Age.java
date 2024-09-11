db5abc8fdf6581fac711d1cd8a8c4da09c43f0b0
Максим <166473866+Macks3891@users.noreply.github.com>
2024-09-11 23:18:46 +0500

    Age new2 Commit

--- src/Age.java
+++ src/Age.java
@@ -3,19 +3,19 @@ import java.util.Scanner;
 public class Age {
     public static void main(String[] args) {
         Scanner agePerson = new Scanner(System.in);
-        String message = "Введите возраст человека.";
-        System.out.println(message);
+        System.out.println("Введите возраст человека:");
 
         int age = agePerson.nextInt();
+        String message = "";
 
         if (age >= 2 && age <= 6) {
-            message = "Иди в детский сад.";
+            message = "Если возраст человека равен " + age + ", то ему нужно идти в детский сад.";
         } else if (age >= 7 && age <= 18) {
-            message = "Иди в школу.";
+            message = "Если возраст человека равен " + age + ", то ему нужно идти в школу.";
         } else if (age > 18 && age < 24) {
-            message = "Уже следует учиться в универе.";
+            message = "Если возраст человека равен " + age + ", то ему нужно учиться в универе.";
         } else if (age >= 24) {
-            message = "Пора на работу.";
+            message = "Если возраст человека равен " + age + ", то ему пора на работу.";
         } else {
             message = "Некорректный возраст.";
         }
